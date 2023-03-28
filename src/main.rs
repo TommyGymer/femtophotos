2f3107a0b31e4d40f163ddbf2b7122cb4ac7556c
TommyGymer <tommygymer@gmail.com>
2023-03-29 00:24:12 +0100

    Init

---
+++ src/main.rs
@@ -0,0 +1,11 @@
+#[macro_use]
+extern create glium;
+
+fn main() {
+    use glium::glutin;
+
+    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+}
