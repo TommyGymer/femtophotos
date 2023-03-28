d02d99430fc80d5cc9cdff9ea513396b18ddf252
TommyGymer <tommygymer@gmail.com>
2023-03-29 00:49:17 +0100

    Image drawing

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,10 @@ extern crate image;
 #[derive(Copy, Clone)]
 struct Vertex {
     position: [f32; 2],
+    tex_coords: [f32; 2],
 }
 
-implement_vertex!(Vertex, position);
+implement_vertex!(Vertex, position, tex_coords);
 
 fn main() {
     use glium::glutin;
@@ -18,10 +19,11 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [ 0.0,  0.5] };
-    let vertex3 = Vertex { position: [ 0.5, -0.25] };
-    let shape = vec![vertex1, vertex2, vertex3];
+    let vertex1 = Vertex { position: [-1.0, -1.0], tex_coords: [0.0, 0.0] };  //bottom left
+    let vertex2 = Vertex { position: [ 1.0, -1.0], tex_coords: [1.0, 0.0] };   //bottom right
+    let vertex3 = Vertex { position: [ 1.0, 1.0], tex_coords: [1.0, 1.0] };     //top right
+    let vertex4 = Vertex { position: [ -1.0, 1.0], tex_coords: [0.0, 1.0] };   //top left
+    let shape = vec![vertex1, vertex2, vertex3, vertex1, vertex4, vertex3];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
@@ -30,29 +32,55 @@ fn main() {
     let vertex_shader_src = r#"
     #version 410
     in vec2 position;
+    in vec2 tex_coords;
+    out vec2 v_tex_coords;
+
+    uniform mat4 matrix;
 
     void main() {
-        gl_Position = vec4(position, 0.0, 1.0);
+        v_tex_coords = tex_coords;
+        gl_Position = matrix * vec4(position, 0.0, 1.0);
     }
     "#;
 
     
     let fragment_shader_src = r#"
     #version 410
+    in vec2 v_tex_coords;
     out vec4 color;
 
+    uniform sampler2D tex;
+
     void main() {
-        color = vec4(1.0, 0.0, 0.0, 1.0);
+        color = texture(tex, v_tex_coords);
     }
     "#;
+
+    use std::io::Cursor;
+    let image = image::load(Cursor::new(&include_bytes!("C:\\Users\\Tom\\Pictures\\20221212_135107.jpg")),
+                            image::ImageFormat::Jpeg).unwrap().to_rgba8();
+    let image_dimensions = image.dimensions();
+    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+
+    let texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
 
     let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
 
     event_loop.run(move |ev, _, control_flow| {
+        let uniforms = uniform! {
+            matrix: [
+                [1.0, 0.0, 0.0, 0.0],
+                [0.0, 1.0, 0.0, 0.0],
+                [0.0, 0.0, 1.0, 0.0],
+                [0.0, 0.0, 0.0, 1.0f32],
+            ],
+            tex: &texture,
+        };
+
         let mut target = display.draw();
         target.clear_color(0.3, 0.3, 0.3, 1.0);
 
-        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms,
+        target.draw(&vertex_buffer, &indices, &program, &uniforms,
             &Default::default()).unwrap();
 
         target.finish().unwrap();
