32e6ac97266b99c215ded0c22629575953271374
TommyGymer <tommygymer@gmail.com>
2023-04-16 20:24:28 +0100

    State struct and rust format

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ extern crate image;
 
 use std::io::Cursor;
 
-use glium::glutin::event::{VirtualKeyCode, ElementState, ModifiersState};
+use glium::glutin::event::{ElementState, ModifiersState, VirtualKeyCode};
 
 #[derive(Copy, Clone)]
 struct Vertex {
@@ -39,65 +39,119 @@ impl Rotation {
         }
     }
 
-    fn to_mat(&self, d_size: (u32, u32), i_size: (u32, u32)) -> [[f32 ; 2] ; 2] {
+    fn to_mat(&self, d_size: (u32, u32), i_size: (u32, u32)) -> [[f32; 2]; 2] {
         match self {
             Rotation::UP => {
                 if ((d_size.0 * i_size.1) as f32 / (d_size.1 * i_size.0) as f32) > 1.0 {
                     [
-                        [(d_size.1 * i_size.0) as f32 / (d_size.0 * i_size.1) as f32, 0.0],
+                        [
+                            (d_size.1 * i_size.0) as f32 / (d_size.0 * i_size.1) as f32,
+                            0.0,
+                        ],
                         [0.0, 1.0],
                     ]
                 } else {
                     [
                         [1.0, 0.0],
-                        [0.0, (d_size.0 * i_size.1) as f32 / (d_size.1 * i_size.0) as f32],
+                        [
+                            0.0,
+                            (d_size.0 * i_size.1) as f32 / (d_size.1 * i_size.0) as f32,
+                        ],
                     ]
                 }
-            },
+            }
             Rotation::RIGHT => {
                 if ((d_size.0 * i_size.0) as f32 / (d_size.1 * i_size.1) as f32) < 1.0 {
                     [
-                        [0.0, (d_size.0 * i_size.0) as f32 / (d_size.1 * i_size.1) as f32],
+                        [
+                            0.0,
+                            (d_size.0 * i_size.0) as f32 / (d_size.1 * i_size.1) as f32,
+                        ],
                         [1.0, 0.0],
                     ]
                 } else {
                     [
                         [0.0, 1.0],
-                        [(d_size.1 * i_size.1) as f32 / (d_size.0 * i_size.0) as f32, 0.0],
+                        [
+                            (d_size.1 * i_size.1) as f32 / (d_size.0 * i_size.0) as f32,
+                            0.0,
+                        ],
                     ]
                 }
-            },
+            }
             Rotation::DOWN => {
                 if ((d_size.0 * i_size.1) as f32 / (d_size.1 * i_size.0) as f32) > 1.0 {
                     [
-                        [(d_size.1 * i_size.0) as f32 / (d_size.0 * i_size.1) as f32, 0.0],
+                        [
+                            (d_size.1 * i_size.0) as f32 / (d_size.0 * i_size.1) as f32,
+                            0.0,
+                        ],
                         [0.0, -1.0],
                     ]
                 } else {
                     [
                         [1.0, 0.0],
-                        [0.0, -((d_size.0 * i_size.1) as f32 / (d_size.1 * i_size.0) as f32)],
+                        [
+                            0.0,
+                            -((d_size.0 * i_size.1) as f32 / (d_size.1 * i_size.0) as f32),
+                        ],
                     ]
                 }
-            },
+            }
             Rotation::LEFT => {
                 if ((d_size.0 * i_size.0) as f32 / (d_size.1 * i_size.1) as f32) < 1.0 {
                     [
-                        [0.0, (d_size.0 * i_size.0) as f32 / (d_size.1 * i_size.1) as f32],
+                        [
+                            0.0,
+                            (d_size.0 * i_size.0) as f32 / (d_size.1 * i_size.1) as f32,
+                        ],
                         [-1.0, 0.0],
                     ]
                 } else {
                     [
                         [0.0, 1.0],
-                        [-((d_size.1 * i_size.1) as f32 / (d_size.0 * i_size.0) as f32), 0.0],
+                        [
+                            -((d_size.1 * i_size.1) as f32 / (d_size.0 * i_size.0) as f32),
+                            0.0,
+                        ],
                     ]
                 }
-            },
+            }
         }
     }
 }
 
 implement_vertex!(Vertex, position, tex_coords);
+
+struct State {
+    rotation: Rotation,
+    directory: String,
+    image_uri: String,
+    modifiers: Option<ModifiersState>,
+    mouse_position: Option<(u32, u32)>,
+    drag_origin: Option<(u32, u32)>,
+}
+
+impl State {
+    fn default() -> Self {
+        Self {
+            rotation: Rotation::UP,
+            directory: String::from(""),
+            image_uri: String::from("C:\\Users\\Tom\\Pictures\\20221212_135107.jpg"),
+            modifiers: None,
+            mouse_position: None,
+            drag_origin: None,
+        }
+    }
+
+    fn next_img(&self) {
+        println!("Next");
+    }
+
+    fn prev_img(&self) {
+        println!("Prev");
+    }
+}
 
 fn main() {
     use glium::glutin;
@@ -108,15 +162,27 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    let vertex1 = Vertex { position: [-1.0, -1.0], tex_coords: [0.0, 0.0] };  //bottom left
-    let vertex2 = Vertex { position: [ 1.0, -1.0], tex_coords: [1.0, 0.0] };   //bottom right
-    let vertex3 = Vertex { position: [ 1.0, 1.0], tex_coords: [1.0, 1.0] };     //top right
-    let vertex4 = Vertex { position: [ -1.0, 1.0], tex_coords: [0.0, 1.0] };   //top left
+    let vertex1 = Vertex {
+        position: [-1.0, -1.0],
+        tex_coords: [0.0, 0.0],
+    }; //bottom left
+    let vertex2 = Vertex {
+        position: [1.0, -1.0],
+        tex_coords: [1.0, 0.0],
+    }; //bottom right
+    let vertex3 = Vertex {
+        position: [1.0, 1.0],
+        tex_coords: [1.0, 1.0],
+    }; //top right
+    let vertex4 = Vertex {
+        position: [-1.0, 1.0],
+        tex_coords: [0.0, 1.0],
+    }; //top left
     let shape = vec![vertex1, vertex2, vertex3, vertex1, vertex4, vertex3];
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
     let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
-    
+
     let vertex_shader_src = r#"
     #version 410
     in vec2 position;
@@ -145,28 +211,44 @@ fn main() {
     }
     "#;
 
-    let image = image::load(Cursor::new(&include_bytes!("C:\\Users\\Tom\\Pictures\\20221212_135107.jpg")),
-                            image::ImageFormat::Jpeg).unwrap().to_rgba8();
+    let program =
+        glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None)
+            .unwrap();
+
+    let image = image::load(
+        Cursor::new(&include_bytes!(
+            "C:\\Users\\Tom\\Pictures\\20221212_135107.jpg"
+        )),
+        image::ImageFormat::Jpeg,
+    )
+    .unwrap()
+    .to_rgba8();
     let image_dimensions = image.dimensions();
-    let image = glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
+    let image =
+        glium::texture::RawImage2d::from_raw_rgba_reversed(&image.into_raw(), image_dimensions);
 
     let texture = glium::texture::SrgbTexture2d::new(&display, image).unwrap();
 
-    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
-
-    let mut rotation = Rotation::UP;
+    let mut state = State::default();
 
     event_loop.run(move |ev, _, control_flow| {
         let uniforms = uniform! {
-            p_rot: rotation.to_mat(display.get_framebuffer_dimensions(), image_dimensions),
+            p_rot: state.rotation.to_mat(display.get_framebuffer_dimensions(), image_dimensions),
             tex: &texture,
         };
 
         let mut target = display.draw();
         target.clear_color(0.3, 0.3, 0.3, 1.0);
 
-        target.draw(&vertex_buffer, &indices, &program, &uniforms,
-            &Default::default()).unwrap();
+        target
+            .draw(
+                &vertex_buffer,
+                &indices,
+                &program,
+                &uniforms,
+                &Default::default(),
+            )
+            .unwrap();
 
         target.finish().unwrap();
 
@@ -178,47 +260,69 @@ fn main() {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
-                    return;
-                },
-                glutin::event::WindowEvent::ModifiersChanged(state) => {
-                    println!("{:?}", state);
-                    return;
-                },
+                }
+                glutin::event::WindowEvent::ModifiersChanged(mod_state) => {
+                    state.modifiers = Some(mod_state);
+                }
+                glutin::event::WindowEvent::CursorMoved { position, .. } => {
+                    state.mouse_position = Some((position.x as u32, position.y as u32));
+                }
                 _ => return,
             },
-            glutin::event::Event::DeviceEvent { device_id: _, event } => match event {
-                glutin::event::DeviceEvent::MouseMotion { delta } => {
-                    println!("{:?}", delta);
-                    return;
-                },
+            glutin::event::Event::DeviceEvent {
+                device_id: _,
+                event,
+            } => match event {
                 glutin::event::DeviceEvent::MouseWheel { delta } => {
                     println!("{:?}", delta);
-                    return;
-                },
-                glutin::event::DeviceEvent::Button { button, state } => {
-                    println!("{:?}: {:#?}", button, state);
-                    return;
+                }
+                glutin::event::DeviceEvent::Button {
+                    button,
+                    state: button_state,
+                } => match (button, button_state) {
+                    (1, ElementState::Pressed) => {
+                        state.drag_origin = state.mouse_position;
+                    }
+                    (1, ElementState::Released) => {
+                        match (state.drag_origin, state.mouse_position) {
+                            (Some(start), Some(end)) => {
+                                if start.0 < end.0 {
+                                    state.prev_img();
+                                } else {
+                                    state.next_img();
+                                }
+                            }
+                            _ => return,
+                        }
+                    }
+                    _ => return,
                 },
                 glutin::event::DeviceEvent::Key(k) => {
-                    match k.virtual_keycode {
-                        Some(VirtualKeyCode::R) => {
-                                if k.state == ElementState::Pressed {
-                                    if k.modifiers.contains(ModifiersState::SHIFT) {
-                                        rotation = rotation.anticlockwise();
-                                    } else {
-                                        rotation = rotation.clockwise();
-                                    }
-                                    println!("rotated!");
-                                    println!("{:?}", rotation);
-                                }
-                                println!("{:?}", k);
-                            },
-                        _ => println!("{:?}", k),
+                    match (k.virtual_keycode, k.state, state.modifiers) {
+                        (Some(VirtualKeyCode::R), ElementState::Pressed, Some(mods)) => {
+                            if mods.contains(ModifiersState::SHIFT) {
+                                state.rotation = state.rotation.anticlockwise();
+                            } else {
+                                state.rotation = state.rotation.clockwise();
+                            }
+                        }
+                        (Some(VirtualKeyCode::R), ElementState::Pressed, None) => {
+                            state.rotation = state.rotation.clockwise();
+                        }
+                        (Some(VirtualKeyCode::Space), ElementState::Pressed, None) => {
+                            state.next_img();
+                        }
+                        (Some(VirtualKeyCode::Right), ElementState::Pressed, None) => {
+                            state.next_img();
+                        }
+                        (Some(VirtualKeyCode::Left), ElementState::Pressed, None) => {
+                            state.prev_img();
+                        }
+                        _ => return,
                     }
-                    return;
-                },
+                }
                 _ => return,
-            }
+            },
             _ => (),
         }
     });
